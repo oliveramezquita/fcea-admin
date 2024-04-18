860cdfb8ff44c5ea564fd29bce12bfaf2b5e8daf
Oliver Amezquita Morales <varilla24@gmail.com>
2024-04-18 13:05:01 -0600

    The page for the monitoring report was added

---
+++ src/pages/reports/monitoring-site-information.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePage({
+  meta: {
+    layout: 'blank',
+    unauthenticatedOnly: true,
+  },
+})
+</script>
+<template>
+  <h1>Información del sitio de monitoreo</h1>
+</template>
