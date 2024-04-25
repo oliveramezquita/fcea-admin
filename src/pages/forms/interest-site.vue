13b1a8f089e5655db5770893317a592e59feb40c
Oliver Amezquita Morales <varilla24@gmail.com>
2024-04-24 19:05:09 -0600

    Fixed issue in sites

--- src/pages/forms/interest-site.vue
+++ src/pages/forms/interest-site.vue
@@ -15,17 +15,23 @@ const emptyProjects = ref(true)
 
 const checkInterestSite = project => {
   const insterestSite = JSON.parse(project.interest_sites_data)
-  if (userData.value.role === 'SUPER_ADMIN')
+  if (userData.value.role === 'SUPER_ADMIN') {
+    emptyProjects.value = projects.value.length ? false : true
     return true
-  if (userData.value.role === 'ADMIN')
+  }
+  if (userData.value.role === 'ADMIN') {
+    emptyProjects.value = !project.admin_users.includes(userData.value._id)
     return project.admin_users.includes(userData.value._id)
-  if (insterestSite.users) 
+  }
+  if (insterestSite.users) {
+    emptyProjects.value = !insterestSite.users.includes(userData.value._id)
     return insterestSite.users.includes(userData.value._id)
+  }
   return false
 }
-projects.value.forEach(project => {
-  emptyProjects.value = !checkInterestSite(project)
-});
+// projects.value.forEach(project => {
+//   emptyProjects.value = !checkInterestSite(project)
+// });
 </script>
 <template>
   <template v-for="project in projects" :key="project._id">
@@ -36,7 +42,7 @@ projects.value.forEach(project => {
   </template>
   <VCard title="Sin cuenca o sitio de interés no asignado" v-if="emptyProjects">
     <VCardText>
-      No cuentas con alguna cuenca activa asignada o algún sitio de interés asignado, para más información envíanos un correo a: <a href="mailto:karla.rivera@fcea.org.mx" target="_blank">karla.rivera@fcea.org.mx</a> o comunícate al teléfono: <a href="https://wa.me/525548104412" target="_blank">55 4810 4412</a>.
+      No cuentas con alguna cuenca asignada o activa, para más información envíanos un correo a: <a href="mailto:info@fcea.org.mx" target="_blank">info@fcea.org.mx</a> o mediante el enlace: <a href="https://fcea.org.mx/contact/" target="_blank">fcea.org.mx/contact/</a>.
     </VCardText>
   </VCard>
 </template>
--- src/pages/forms/reference-site.vue
+++ src/pages/forms/reference-site.vue
@@ -15,23 +15,29 @@ const emptyProjects = ref(true)
 
 const checkReferenceSite = project => {
   const referenceSite = JSON.parse(project.reference_sites_data)
-  if (userData.value.role === 'SUPER_ADMIN')
+  if (userData.value.role === 'SUPER_ADMIN') {
+    emptyProjects.value = projects.value.length ? false : true
     return true
-  if (userData.value.role === 'ADMIN')
+  }
+  if (userData.value.role === 'ADMIN') {
+    emptyProjects.value = !project.admin_users.includes(userData.value._id)
     return project.admin_users.includes(userData.value._id) 
-  if (referenceSite.users) 
+  }
+  if (referenceSite.users) {
+    emptyProjects.value = !referenceSite.users.includes(userData.value._id)
     return referenceSite.users.includes(userData.value._id)
+  }
   return false
 }
-projects.value.forEach(project => {
-  emptyProjects.value = !checkReferenceSite(project)
-});
+// projects.value.forEach(project => {
+//   emptyProjects.value = !checkReferenceSite(project)
+// });
+
 </script>
 <template>
   <template
     v-for="project in projects" 
-    :key="project._id" 
-    v-if="!emptyProjects">
+    :key="project._id">
     <SiteReferenceInfo
       :project-data="project"
       :user-data="userData"
@@ -39,7 +45,7 @@ projects.value.forEach(project => {
   </template>
   <VCard title="Sin cuenca asignada" v-if="emptyProjects">
     <VCardText>
-      No cuentas con alguna cuenca asignada o activa, para más información envíanos un correo a: <a href="mailto:karla.rivera@fcea.org.mx" target="_blank">karla.rivera@fcea.org.mx</a> o comunícate al teléfono: <a href="https://wa.me/525548104412" target="_blank">55 4810 4412</a>.
+      No cuentas con alguna cuenca asignada o activa, para más información envíanos un correo a: <a href="mailto:info@fcea.org.mx" target="_blank">info@fcea.org.mx</a> o mediante el enlace: <a href="https://fcea.org.mx/contact/" target="_blank">fcea.org.mx/contact/</a>.
     </VCardText>
   </VCard>
 </template>   
